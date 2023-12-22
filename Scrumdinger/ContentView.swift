de13919ff915038e34568ff3ee8aaeba99ca8001
Ibrar-ghani <ibrar.ghani.aaur@gmailcom>
2023-12-22 12:02:16 +0500

    Initial Commit

---
+++ Scrumdinger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by user on 22/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
